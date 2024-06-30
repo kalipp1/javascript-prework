e1739a4a6116acc257ff1e5e3d1c224b23faebcf
Maks Birecki <maksymilian.birecki@gmail.com>
2024-06-30 20:17:30 +0200

    ResultScriptAdd

--- js/script.js
+++ js/script.js
@@ -24,8 +24,30 @@ if(playerInput == '1'){
   playerMove = 'kamień';
 } else if (playerInput == '2'){
     playerMove = 'papier';
-} else {
+} else if (playerInput == '3'){
     playerMove = 'nożyce';
 } ;
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  } else if ( computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Wygrywa Komputer!');
+  } else if (computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis!');
+  } else if (computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Komputer wygrywa!');
+  } else if (computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis!');
+  } else if (computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  } else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  } else if (computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Komputer wygrywa!');
+  } else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis!');
+  } else {
+    printMessage('Wybierz poprawny ruch!!!');
+  };
