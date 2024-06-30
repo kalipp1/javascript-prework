598f47c7b16d39e3038c9c3f85657aa8a16646ba
Maks Birecki <maksymilian.birecki@gmail.com>
2024-06-30 21:10:49 +0200

    ResultScriptAddUpdate

--- js/script.js
+++ js/script.js
@@ -1,3 +1,68 @@
+// console.log('moves:', argComputerMove, argPlayerMove);
+
+// let argMoveId = Math.floor(Math.random() * 3 + 1);
+
+
+// function getMoveName(argMoveId){
+//     if(argMoveId == 1){
+//       return 'kamień';
+//     } else if(argMoveId == 2){
+//         return 'papier';
+//     } else if (argMoveId == 3){
+//         return 'nożyce';
+//     }
+
+//     printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+//     return 'nieznany ruch';
+//   }
+
+
+
+// console.log('Wylosowana liczba to: ' + randomNumber);
+
+// let argComputerMove = getMoveName(argMoveId);
+
+// let argPlayerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+// console.log('Gracz wpisał: ' + argPlayerInput);
+
+// let argPlayerMove = getMoveName(argPlayerInput);
+
+// function displayResult (argComputerMove, argPlayerMove){
+//     printMessage('Komputer zagrał '+ argComputerMove + 'a ty ' + argPlayerMove);
+
+//     if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+//         return 'Ty wygrywasz!';
+//       } else if ( argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+//         return 'Wygrywa Komputer!';
+//       } else if ( argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
+//         return 'Remis!';
+//       } else if ( argComputerMove == 'papier' && argPlayerMove == 'kamień'){
+//         return 'Komputer wygrywa!';
+//       } else if ( argComputerMove == 'papier' && argPlayerMove == 'papier'){
+//         return 'Remis!';
+//       } else if ( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+//         return 'Ty wygrywasz!';
+//       } else if ( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+//         return 'Ty wygrywasz!';
+//       } else if ( argComputerMove == 'nożyce' && argPlayerMove == 'papier'){
+//         return 'Komputer wygrywa!';
+//       } else if ( argComputerMove == 'nożyce' && argPlayerMove == 'nożyce'){
+//         return 'Remis!';
+//       }
+//       printMessage('Niepoprawnie zagrana gra');
+//       return 'Zagraj poprawny ruch';
+    
+// }
+
+// let display = displayResult();
+
+// printMessage('Wynik gry: ' + display);
+
+// console.log('result :', display);
+
+// Działa
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
